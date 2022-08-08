baffc24ec1dfd2632d09067305a3e03c7b33878d
codeotter0201 <codeotter0201@gmail.com>
2022-08-08 15:20:30 +0800

    last changed at 2022-08-08 15:20,pushed by Me

--- push_to_github_commit_auto.py
+++ push_to_github_commit_auto.py
@@ -1,7 +1,7 @@
 import os
 import datetime
 now = datetime.datetime.now()
-now = f'last changed at {str(now.strftime("%Y-%m-%d %H:%M"))},pushed by codeotter'
+now = f'last changed at {str(now.strftime("%Y-%m-%d %H:%M"))},pushed by Me'
 os.system("git pull origin master")
 os.system("git add .")
 os.system(f'git commit -m "{now}"')
